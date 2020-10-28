c470b4a155c40aa960aef981e1415d54d92179f6
Mirpribili <mirpribili@ya.ru>
2020-10-28 17:08:42 +0300

    add git simple project

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,7 @@
 from .help import dp
 from .start import dp
+# echo перезватывает все текстовые сообщения
+# и если поместить выше старта то старт не выполнится никогда
 from .echo import dp
-
+# порядок важен!
 __all__ = ["dp"]
