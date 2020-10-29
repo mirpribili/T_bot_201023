82a6f48208042425262a4db458224d88159135ec
Mirpribili <mirpribili@ya.ru>
2020-10-29 14:34:03 +0300

    урок 21  пройден

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -8,12 +8,26 @@
 from aiogram import types
 from loader import dp
 
+# ПОРЯДОК ИМЕЕТ ЗНАЧЕНИЕ 1й ТОТ ЧТО ВЫШЕ
+
+
 # 1й способ регистрации хендлера
-#dp.register_message_handler()
+#dp.register_message_handler(наша ф-я)
 
 # 2й способ регистрации хендлера
-@dp.message_handler()
+@dp.message_handler() # по умолчанию чюда летят content_types=types.ContentTypes.TEXT
+# Если хотите что то другое ловить то указывайте тип явно типа
+# content_types==types.ContentTypes.PHOTO
+#
+# допустим нужно ловить channel_post тогда
+# @dp.channel_post_handler()
 async def bot_echo(message: types.Message):
+    # тайп хинт для подсказок))
+    # types.Message) а тут можно найти всет ипы объектов доступные в телеграме)
+    #message. ...
+
+    # Эхо бот)
+
     # Получим chat_id и text
     chat_id = message.from_user.id
     text = message.text
@@ -36,3 +50,5 @@ async def bot_echo(message: types.Message):
 
     # Отправим сообщение пользователю - вариант 3 (с реплаем)
     # await message.reply(text=text)
+    # аналог
+    # await bot.send_message(chat_id=chat_id, text=text, reply_to_message_id=message.message_id)
